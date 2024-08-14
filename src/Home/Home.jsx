f043b24dc576ad0761a5d7c60198bd716a5023e1
Vitor Souza <164961597+Carcara007@users.noreply.github.com>
2024-08-14 16:07:27 -0300

    Add files via upload

---
+++ src/Home/Home.jsx
@@ -0,0 +1,17 @@
+import styles from './Home.module.css'
+import SideBar from '../components/SideBar/SideBar'
+import Game from '../components/Game/Game'
+
+function Home() {
+
+  return (
+    <>
+
+        
+        <Game/>
+ 
+    </>
+  )
+}
+
+export default Home
